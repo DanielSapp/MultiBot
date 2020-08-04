1faa0245e65aec893b1a92e80da5883ddabf3c72
Daniel Sapp <enipony@gmail.com>
2020-08-03 21:34:35 -0500

    Added method outlines to RoleHandler.  Commpleted RoleHandler.listRoles().  Completed WolframHandler.

--- src/main/java/Roles/RoleHandler.java
+++ src/main/java/Roles/RoleHandler.java
@@ -1,7 +1,7 @@
 package Roles;
 
 import MessageHandling.QueryHandler;
-import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.*;
 
 public class RoleHandler extends QueryHandler {
 
@@ -10,6 +10,38 @@ public class RoleHandler extends QueryHandler {
      */
     @Override
     public void handleQuery(Message message) {
+        String messageText = message.getContentStripped();
+        if (messageText.startsWith("!list")) {
+            listRoles(message.getGuild(), message.getChannel());
+        } else if (messageText.startsWith("!add")) {
+            addRoleToUser(message.getAuthor(), messageText.substring(messageText.indexOf(" ")+1));
+        } else if (messageText.startsWith("!remove")) {
+            removeRoleFromUser(message.getAuthor(), messageText.substring(messageText.indexOf(" ")+1));
+        } else if (messageText.startsWith("!create")) {
+            createRole(message);
+        }
+    }
 
+    private static void listRoles(Guild guild, MessageChannel channel) {
+        StringBuilder sb = new StringBuilder();
+        for (Role r : guild.getRoles()) {
+            if (r.getName().equals("@everyone")) {
+                continue;
+            }
+            sb.append(r.getName());
+            sb.append("\n");
+        }
+        channel.sendMessage("Roles:\n" + sb.toString()).queue();
+    }
+    private static void addRoleToUser(User user, String roleName) {
+        //TODO
+    }
+
+    private static void removeRoleFromUser(User user, String roleName) {
+        //TODO
+    }
+
+    private static void createRole(Message message) {
+        //TODO
     }
 }
--- src/main/java/Wolfram/WolframHandler.java
+++ src/main/java/Wolfram/WolframHandler.java
@@ -3,6 +3,14 @@ package Wolfram;
 import MessageHandling.QueryHandler;
 import net.dv8tion.jda.api.entities.Message;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 public class WolframHandler extends QueryHandler {
     private String appID;
 
@@ -17,8 +25,29 @@ public class WolframHandler extends QueryHandler {
      * @param message The user message that contained this query.
      */
     @Override
-    public void handleQuery(Message message) {
+    public void handleQuery (Message message)  {
         String messageText = message.getContentStripped();
         String query = messageText.substring(messageText.indexOf("!wolfram")+8).trim();
+        String responseText;
+        try {
+            responseText = getQueryAnswer(query);
+        } catch (IOException e) {
+            System.out.println("An error occurred while executing a Wolfram Alpha query");
+            e.printStackTrace();
+            return;
+        }
+        message.getChannel().sendMessage(responseText).queue();
+    }
+    private String getQueryAnswer(String query) throws IOException {
+        URL url = new URL("http://api.wolframalpha.com/v1/result?appid=" + appID + "&i=" + URLEncoder.encode(query, StandardCharsets.UTF_8));
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder responseText = new StringBuilder();
+        String nextLine;
+        while ((nextLine = br.readLine()) != null) {
+            responseText.append(nextLine);
+        }
+        return responseText.toString();
     }
 }
