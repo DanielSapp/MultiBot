7cd8c191eedbd5ba6885e94e847e1c9d9572d142
Daniel Sapp <enipony@gmail.com>
2020-08-04 20:00:00 -0500

    Actually added the logic to RemoveRoleQueryHandler.

--- src/main/java/QueryHandlers/Roles/RemoveRoleQueryHandler.java
+++ src/main/java/QueryHandlers/Roles/RemoveRoleQueryHandler.java
@@ -3,6 +3,8 @@ package QueryHandlers.Roles;
 import QueryHandlers.QueryHandler;
 import net.dv8tion.jda.api.entities.*;
 
+import java.util.List;
+
 public class RemoveRoleQueryHandler extends QueryHandler {
 
     /**
@@ -18,6 +20,30 @@ public class RemoveRoleQueryHandler extends QueryHandler {
 
     @Override
     public void handleQuery(Message message) {
-
+        Member member = message.getMember();
+        String messageText = message.getContentStripped();
+        MessageChannel mc = message.getChannel();
+        String roleName = messageText.substring(messageText.indexOf(" ")+1);
+        List<Role> matchingRoles = message.getGuild().getRolesByName(roleName, true);
+        if (matchingRoles.size() == 1) {
+            if (member.getRoles().contains(matchingRoles.get(0))) {
+                message.getGuild().removeRoleFromMember(member, matchingRoles.get(0));
+                System.out.println("Role " + roleName + " has been removed from " + member.getNickname() + " in " + message.getGuild() + ".");
+                mc.sendMessage(matchingRoles.get(0).getName() + " has been removed from " + member.getNickname() + ".").queue();
+            } else {
+                System.out.println("Error: " + member.getNickname() + " in " + message.getGuild() + " tried to remove Role " + roleName + " that they don't have.");
+                mc.sendMessage("Error: " + member.getNickname() + " does not have the Role " + matchingRoles.get(0).getName()).queue();
+            }
+        } else if (matchingRoles.size() == 0) {
+            System.out.println("Error: " + member.getNickname() + " in " + message.getGuild() + " tried to remove Role " + roleName + " but more than one Role matched it");
+            StringBuilder messageBuilder = new StringBuilder();
+            messageBuilder.append("Error: all of the following Roles match " + roleName + "\n");
+            for (Role r : matchingRoles) {
+                messageBuilder.append(r.getName() + "\n");
+            }
+            mc.sendMessage(messageBuilder).queue();
+        } else {
+            System.out.println("Error: " + member.getNickname() + " in " + message.getGuild() + " tried to remove Role " + roleName + " but no Roles matched it.");
+        }
     }
 }
